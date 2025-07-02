28f2db03cda93240ba8c4af5e90b87f77549e9a7
waregamo <waregamoses20@gmail.com>
2025-07-02 17:47:31 +0300

    Add CREATE INDEX statements for performance optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -12,3 +12,38 @@ CREATE INDEX idx_properties_id ON properties(id);
 
 -- Create index on reviews.property_id for LEFT JOIN and filtering
 CREATE INDEX idx_reviews_property_id ON reviews(property_id);
+
+
+
+## Query Performance Analysis: Before and After Indexing
+
+### Initial Query (Before Indexes)
+
+EXPLAIN ANALYZE
+SELECT ...
+ b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number,
+
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
